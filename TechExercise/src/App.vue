7234699e7889ad6ebb716c1df322f6f8809d178c
Ryan <139078564+Acuna0@users.noreply.github.com>
2024-02-22 15:22:53 -0600

    first commit

---
+++ TechExercise/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView />
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+</template>
+
+<style scoped>
+</style>
---
+++ TechExercise/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>This is an About Page</h1>
+  </div>
+</template>
+
+<style>
+</style>
